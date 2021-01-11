b0e1a4cad10bfc46f1f5a8cd4b6d5e9f09086877
Sam Girshovich <sam.girshovich@gmail.com>
2021-01-11 15:15:25 -0500

    user already exists

--- src/api/database/db.ts
+++ src/api/database/db.ts
@@ -4,18 +4,27 @@ const admin = require('firebase-admin');
 import serviceAccount from './serviceAccountKeys.json';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
 
 const db: any = admin.firestore();
+console.log(typeof db)
 
 const users = db.collection('users');
+console.log(typeof users)
 
-export function signin(user:User){
-    users.doc(user.id).set({
-        id:    user.id,
-        name:  user.name,
-        email: user.email,
-        photo: user.email
-    });  
+export function signin(user:User) {
+    let doc = users.doc(user.id).get()
+    if (doc.exists) {
+        // user does exist
+        console.log('user exists')
+    } else {
+        // user does not exist
+        users.doc(user.id).set({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            photo: user.photo
+          });
+    }
 }
